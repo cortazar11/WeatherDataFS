b11132e8a857a592087ccd8959b6b3962e005c55
Miguel Martinez <miguel.martinez11@gmail.com>
2020-10-12 20:16:25 +0200

    second commit

--- server/index.js
+++ server/index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5001);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('Server running...');
+});
