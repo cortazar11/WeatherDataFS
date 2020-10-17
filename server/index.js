24bc071082257e9a205f5021604d72b62918fb86
Miguel Martinez <miguel.martinez11@gmail.com>
2020-10-17 18:14:14 +0200

    finsih authflow

--- server/index.js
+++ server/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const mongoose=require('mongoose');
+const cookieSession=require('cookie-session');
+const passport= require('passport');
+require('./models/User');
+require('./services/passport')
+
+const authRoutes=require('./routes/authRoutes');
+const keys=require('./config/keys')
 
 const app = express();
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}))
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'another there' });
-});
+app.use(passport.initialize())
+app.use(passport.session())
+
+authRoutes(app)
+
+mongoose.connect(keys.mongoURI, { useUnifiedTopology: true,useNewUrlParser: true },()=>{
+  console.log('mongo connected')
+})
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('Server running...');
 });
+
+// Database user: cortazar11
+// Database password: BgwM1R12ERg6OS9B
+// mongodb+srv://cortazar11:<password>@cluster0.qgmwi.mongodb.net/<dbname>?retryWrites=true&w=majority
